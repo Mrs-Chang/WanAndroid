6e13be050e423247da8629742ce269bc34ff5f0e
GaoChang <gaochang@192.168.0.101>
2023-07-02 17:17:35 +0800

    同步代码

--- app/src/main/java/com/chang/android/MainActivity.kt
+++ app/src/main/java/com/chang/android/MainActivity.kt
@@ -5,14 +5,12 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.chang.android.databinding.ActivityMainBinding
 import com.chang.android.module_framework.base.BaseViewBindActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseViewBindActivity<ActivityMainBinding>() {
     override fun initView(savedInstanceState: Bundle?) {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         //NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        bottomNavigationView.setupWithNavController(navController) // 与上面一行代码等价
+        binding.bottomNavView.setupWithNavController(navController) // 与上面一行代码等价
     }
 }
